273dccd4bc28367dc2cbbc65685d4052eca4f695
DeliaCerecedo <dhdiaz1971@gmail.com>
2022-08-10 10:13:27 -0500

    HU1 background

--- src/data.js
+++ src/data.js
@@ -1,9 +1,47 @@
 // estas funciones son de ejemplo
+//import data from './data/ghibli/ghibli.js';
 
+/*
 export const example = () => {
   return 'example';
 };
-
+*/
 export const anotherExample = () => {
   return 'OMG';
 };
+
+
+
+/*
+export let titles = "";
+  data.films.forEach(({title}) => {
+    titles += title;
+   // return title;
+    //console.log(titles);
+  });
+ 
+  
+*/
+ 
+ 
+  
+ 
+
+
+
+
+/*
+
+export let getTitles = () => {   //aquí obtengo los datos y los títulos de las películas
+  let titles = "";   
+  data.films.forEach(({title}) => {
+     titles = title;
+    console.log(titles);
+  return title;
+});
+return titles;
+}
+*/
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import { example } from './data.js';
+//import { titles }  from './data.js';
 
-console.log(example, data);
+import data from './data/ghibli/ghibli.js';
+// import data from './data/rickandmorty/rickandmorty.js';
+//ÚNICAMENTE DOM 
+//console.log(example, data);
+
+console.log(data.films);
+
+
+
+
+let titles = [];
+  data.films.forEach(({title}) => {
+    titles += title;
+   // return title;
+    //console.log(titles);
+  });
+ 
+document.getElementById("main").innerHTML = titles; 
