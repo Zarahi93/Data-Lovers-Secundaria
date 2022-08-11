2c9b5672c28b8f78655b0e3ae7bf1bbb35010cb4
DeliaCerecedo <dhdiaz1971@gmail.com>
2022-08-11 06:13:19 -0500

    creación de nodos por paquete: poster, title, year

--- src/main.js
+++ src/main.js
@@ -1,11 +1,50 @@
 //import { example } from './data.js';
-//import { titles }  from './data.js';
+
+// import data from './data/rickandmorty/rickandmorty.js';
 
 import data from './data/ghibli/ghibli.js';
 
-// import data from './data/rickandmorty/rickandmorty.js';
-//ÚNICAMENTE DOM 
-//console.log(example, data);
+
+//*********** 4 se crean los nodos por paquete 
+
+let posters = data.films.map(({poster}) => poster);
+let titles = data.films.map(({title}) => title)
+let years = data.films.map(({release_date}) => release_date)
+const fatherNode=document.querySelector("main");
+
+for (let i = 0; i <posters.length; i++) {
+  const posterOne=document.createElement("img");
+  const titleOne=document.createElement("p");
+  const dateOne=document.createElement("p"); 
+  const photo = posters[i];
+  posterOne.setAttribute("src", photo);  
+  titleOne.innerHTML=titles[i];
+  dateOne.innerHTML=years[i];
+  fatherNode.append(posterOne, titleOne, dateOne);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *****1********* 1 prueba de crear nodos  y elementos de UN solo dato 
+
 
 //console.log(data.films);
 //console.log(data.films[0].title, data.films[0].release_date, data.films[0].poster);
@@ -34,41 +73,7 @@ import data from './data/ghibli/ghibli.js';
 
 
 
-//***********se crean los nodos por separado 
-
-let posters = data.films.map(({poster}) => poster);
-const fatherNode=document.querySelector("main");
-
-for (let i = 0; i <posters.length; i++) {
-  const posterOne=document.createElement("img"); 
-  const photo = posters[i];
-  posterOne.setAttribute("src", photo);
-  fatherNode.append(posterOne);
-}
-
-
-let titles = data.films.map(({title}) => title);
-
-for (let i = 0; i < titles.length; i++) {
-  const titleOne=document.createElement("p");
-  titleOne.innerHTML=titles[i];
-  fatherNode.append(titleOne);
-}
-
-
-let years = data.films.map(({release_date}) => release_date);
-
-for (let i = 0; i < years.length; i++) { 
-  const dateOne=document.createElement("p");
-  dateOne.innerHTML=years[i];
-  fatherNode.append(dateOne);
-}
-
-
-
-
-
-//*********** con map obtengo un arreglo ***********************
+//*********** 2 con map obtengo un arreglo con todas las peliclas, otro con todos los títulos ***********************
 // let posters = data.films.map(({poster}) => poster);
 // console.log(posters);
 
@@ -79,10 +84,7 @@ for (let i = 0; i < years.length; i++) {
 // console.log(years);
 
 
-
-
-
-//********************el método forEach me guardo un arrego de objetos ********
+//******************** 2 el método forEach me guardo un arrego de objetos ********
 
 // let posters=[];
 // data.films.forEach(({poster}) => posters.push({poster}));
@@ -95,4 +97,40 @@ for (let i = 0; i < years.length; i++) {
 // let years=[];
 // data.films.forEach(({release_date}) => years.push({release_date}));
 // console.log(years);
+
+
+
+//*********** 3 se crean los nodos separado en grupo:
+
+// let posters = data.films.map(({poster}) => poster);
+// const fatherNode=document.querySelector("main");
+
+// for (let i = 0; i <posters.length; i++) {
+//   const posterOne=document.createElement("img"); 
+//   const photo = posters[i];
+//   posterOne.setAttribute("src", photo);
+//   fatherNode.append(posterOne);
+// }
+
+
+// let titles = data.films.map(({title}) => title);
+
+// for (let i = 0; i < titles.length; i++) {
+//   const titleOne=document.createElement("p");
+//   titleOne.innerHTML=titles[i];
+//   fatherNode.append(titleOne);
+// }
+
+
+// let years = data.films.map(({release_date}) => release_date);
+
+// for (let i = 0; i < years.length; i++) { 
+//   const dateOne=document.createElement("p");
+//   dateOne.innerHTML=years[i];
+//   fatherNode.append(dateOne);
+// }
+
+
+
+
 
