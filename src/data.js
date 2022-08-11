f7baba404b73c214cc6b77c17aa50581365e1ba3
DeliaCerecedo <dhdiaz1971@gmail.com>
2022-08-11 06:14:31 -0500

    nodos añadidos con append para crear los paquetes de cada película

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 // estas funciones son de ejemplo
-//import data from './data/ghibli/ghibli.js';
+
 
 /*
 export const example = () => {
@@ -10,38 +10,6 @@ export const anotherExample = () => {
   return 'OMG';
 };
 
-
-
-/*
-export let titles = "";
-  data.films.forEach(({title}) => {
-    titles += title;
-   // return title;
-    //console.log(titles);
-  });
- 
-  
-*/
- 
- 
-  
- 
-
-
-
-
-/*
-
-export let getTitles = () => {   //aquí obtengo los datos y los títulos de las películas
-  let titles = "";   
-  data.films.forEach(({title}) => {
-     titles = title;
-    console.log(titles);
-  return title;
-});
-return titles;
-}
-*/
 
 
 
