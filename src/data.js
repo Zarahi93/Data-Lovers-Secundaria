669a869ef6eebbc4135ac0c7d1393523223b6021
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2022-08-31 00:39:57 -0500

    Comentarios

--- src/data.js
+++ src/data.js
@@ -1,54 +1,47 @@
 
-export const filter = (filterSelected, filterClassName, datas) => {
-  //console.log(filterSelected, filterClassName);
+//*****Funcion para el filtrado*****//
+export const filter = (filterSelected, filterClassName, datas) => {//Se exporta y delcara la función
     let filterData = [];
     if(filterClassName==="director"){
-      //console.log("director" + filterSelected);
 
-      filterData = datas.filter(({director}) => director===filterSelected);
-      //console.log(filterData);
+      filterData = datas.filter(({director}) => director===filterSelected);//Filtra segun el nombre del director seleccionado
      
     }else if(filterClassName==="producer"){
-      //console.log("producer" + filterSelected);
 
       filterData = datas.filter(({producer}) => producer===filterSelected);
-    //console.log(filterData);
       
 
-    }else if(typeof filterSelected === "string" && filterClassName==="year") {
-     // console.log(typeof filterSelected);
-      filterSelected=filterSelected.split(','); 
-      //console.log(filterSelected);
+    }else if(typeof filterSelected === "string" && filterClassName==="year") {//la variable debe ser de tipo string y el nombre de la calse debe ser year
+      filterSelected=filterSelected.split(','); //Divide el string en substrings usando el separador , y las devuelve como arreglo
       
       let firstDate=filterSelected[0];
-      let lastDate=filterSelected.slice(-1)[0].trim();
-      filterData = datas.filter(({release_date}) => (release_date>=firstDate && release_date<=lastDate));
+      let lastDate=filterSelected.slice(-1)[0].trim();//devuelve una copia de una parte del array dentro de
+      // un nuevo array de inicio a fin y elimina los espacios en blanco en ambos extremos del string
+      filterData = datas.filter(({release_date}) => (release_date>=firstDate && release_date<=lastDate));//Los años deben estar dentro de un rango
     }
     return filterData;
 
   }
 
 
-
-export const order = (orderSelected, datas) => {
+//*****Función para ordenar las películas*****//
+export const order = (orderSelected, datas) => {//Declara y exporta la función
   let orderData = [];
 
   if(orderSelected==="upward"){
-    orderData = [...datas].sort((x, y) => (x.title<y.title ? -1:1)); 
+    orderData = [...datas].sort((x, y) => (x.title<y.title ? -1:1)); //Ordena de la A-Z segun el titulo 
       
   }else if(orderSelected==="downward"){
-    orderData = [...datas].sort((x, y) => (x.title>y.title ? -1:1));  
+    orderData = [...datas].sort((x, y) => (x.title>y.title ? -1:1)); //Ordena de la Z-A segun el titulo
   } 
   return orderData;  
 
 }
 
 
-
-export const stadistics = (filterSelected, datas) => {
-  // if (filterSelected==="hola" && datas === "hola"){
-  //     throw new TypeError ("Debes enviar 2 argumentos");
-  // }
+//*****Función que realiza el calculo estadistico (promedio)*****//
+export const stadistics = (filterSelected, datas) => {//Declara y exporta la función
+ 
   
   let total = datas.length;
   
@@ -58,35 +51,27 @@ export const stadistics = (filterSelected, datas) => {
   let filterData = datas.filter(({release_date}) => (release_date>=firstDate && release_date<=lastDate));
 
   let moviesDecade=filterData.length;
-  let average=(moviesDecade/total)*100;
-   //console.log(`El porcentaje de películas relaizadas en esta década es: ${average}%`);
-     
+  let average=(moviesDecade/total)*100;//Calcula el porcentaje de peliculas estrenadas por año     
      return average;
 }
 
-  
+//*****Función que busca dentro de la data la información ingresada*****//
 export const search = (textForSearch, filterSelected, datas) => {
-  //console.log(textForSearch, filterSelected, datas);
   let filterData = [];
    if (filterSelected==="directors"){
-    //console.log(textForSearch);
     filterData = datas.filter(({director}) => director===textForSearch);
-    //console.log(filterData);
 
    }else if(filterSelected==="producers"){
     
     filterData = datas.filter(({producer}) => producer===textForSearch);
-    //console.log(filterData);
 
    }else if(filterSelected==="titles"){
  
     filterData = datas.filter(({title}) => title===textForSearch);
-    //console.log(filterData);
 
    }else if(filterSelected==="years"){
 
     filterData = datas.filter(({release_date}) => release_date===textForSearch);
-    //console.log(filterData);
   }
   return filterData;
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
-//import { example } from './data.js';
-
-// import data from './data/rickandmorty/rickandmorty.js';
-
-import data from './data/ghibli/ghibli.js';
-import { filter, order, search, stadistics } from './data.js';
+import data from './data/ghibli/ghibli.js';//Importa toda la data de Ghibli
+import { filter, order, search, stadistics } from './data.js'; //Importa las funciones desde data.js
 
 //******Se declaran las variables a utilizar********
 let datas=data.films;
-let selectedData=[];//Se asigna un arreglo vacío a la variable
-let posters = [];
+let selectedData=[];  //Se asigna un arreglo vacío a la variable
+let posters = [];    //para despues poder reasignarles otro arreglo
 let titles = []; 
 let years = [];
 let recap = [];
@@ -16,7 +12,7 @@ let directors = [];
 let producers = [];
 
 let posterPicture="";//Se le asigna un string vacio a la variable
-let titlePicture="";
+let titlePicture="";//para despues poder reasignarles otro string
 let yearPicture=""; 
 let recapPicture="";
 let directorPicture="";
@@ -24,20 +20,17 @@ let producerPicture="";
 let newArt="";
 const fatherNode=document.querySelector("main");//Se obtiene y asigna la sección main del documento a la variable fatherNode
 
-//******Funcion que crea los arreglos con la informacion de todas las peliculas*******/
+//******Función que crea los arreglos con la informacion de todas las peliculas*******//
 function mapData(){
-  const dataOriginal = data.films;
-  //console.log(dataOriginal)
-  posters = dataOriginal.map(({poster}) => poster);//Cre aun nuevo arreglo con todos los "poster" que se encuentran dentro del objeto "films" de cada pelicula en "posters"
-  titles = dataOriginal.map(({title}) => title);
-  //console.log(titles);
-  years = dataOriginal.map(({release_date}) => release_date);
-  recap = dataOriginal.map(({description}) => description);
-  directors = dataOriginal.map(({director}) => director);
-  producers = dataOriginal.map(({producer}) => producer);
-
+const dataOriginal = data.films;//Se declara una variable y se le asigna un arreglo de donde obtener los datos
+posters = dataOriginal.map(({poster}) => poster);            //map crea un nuevo arreglo apartir de los resultados de la
+titles = dataOriginal.map(({title}) => title);              //llamada a la función indicada aplicados a cada uno de sus
+years = dataOriginal.map(({release_date}) => release_date);//elementos.
+recap = dataOriginal.map(({description}) => description); 
+directors = dataOriginal.map(({director}) => director);
+producers = dataOriginal.map(({producer}) => producer);
 }
-//*****Funcion que crea los elementos html donde ira la info de las peliculas *****/
+//*****Función que crea los elementos html donde ira la info de las peliculas*****/
 function showMovies () {
   for (let i = 0; i <posters.length; i++) { //El for es para indicar que debe repetir las acciones para cada arreglo
     posterPicture=document.createElement("img");//Se crea un elemento en el documento de tipo imagen
@@ -49,7 +42,7 @@ function showMovies () {
     newArt=document.createElement("article");
     posterPicture.setAttribute("src", posters[i]);  //Se agina el atributo de tipo src y se le da el link del poster
     posterPicture.setAttribute("class","posterMovie")
-    titlePicture.innerHTML=titles[i];//Se muestra el titulo de la pelicula en el documento html
+    titlePicture.innerHTML=titles[i];//Se muestra el titulo de cada película en el documento html
     titlePicture.setAttribute("class", "titleMovie");
     yearPicture.innerHTML=years[i];
     yearPicture.setAttribute("class", "yearMovie");
@@ -60,90 +53,78 @@ function showMovies () {
     directorPicture.setAttribute("class", "directorMovie");
     producerPicture.innerHTML="Producer: " + producers[i];
     producerPicture.setAttribute("class", "producerMovie");
-    fatherNode.append(newArt);//Se le dice que se ponga el nuevo article despues del anterior.
-    newArt.append(posterPicture, titlePicture, yearPicture,recapPicture,directorPicture,producerPicture);//se le dice que dentro del article agregue la información las peliculas
-    //console.log(newArt);
-  
-  
+    fatherNode.append(newArt);//Se le dice que se ponga el nuevo article despues del anterior (uno tras otro).
+    newArt.append(posterPicture, titlePicture, yearPicture,recapPicture,directorPicture,producerPicture);//se le dice que dentro del article agregue la información las peliculas  
   }
 
 }
-
+//*****Función que muestra el contenido ya filtrado/ordenado y search*****//
 function showFIlterOrOrderSearch(){
-  fatherNode.innerHTML="";
-  posters = selectedData.map(({poster}) => poster);//Creaun nuevo arreglo con todos los "poster" que se encuentran dentro del objeto "films" de cada pelicula en "posters"
-  titles = selectedData.map(({title}) => title)
+  fatherNode.innerHTML="";//Se "borran" (string vacio) del html los datos originales
+  posters = selectedData.map(({poster}) => poster);//apartir de los datos filtrados/ordenados crea un nuevo arreglo
+  titles = selectedData.map(({title}) => title)   
   years = selectedData.map(({release_date}) => release_date)
   recap = selectedData.map(({description}) => description)
   directors = selectedData.map(({director}) => director)
   producers = selectedData.map(({producer}) => producer)
-  showMovies();
+  showMovies();//manda a llamar la función que crea los elementos html donde ira la info de las peliculas
   }
 
-
+//*****Función que hace el calculo estadistico y lo muestra en la página*****/
 function showStadistic(){
   fatherNode.innerHTML=""; 
-  
-    // console.log(`El porcentaje de películas relaizadas en esta década es: ${selectedData}%`); 
     let titleStadistic = document.createElement("p");
     titleStadistic.setAttribute("class", "titleStadistic");
     titleStadistic.innerHTML=`Studio Ghibli has released 20 movies. <br><br> The percentage of films released in this decade is:  ${selectedData}%`;
-    fatherNode.append(titleStadistic);
+    fatherNode.append(titleStadistic);//Crea el elemento p dentro de la sección main
     
 }
 
-mapData();
+mapData();//Manda a llamar la función que crea los arreglos con la informacion de todas las peliculas
 showMovies();
 
 
-//      *************AQUI  se leen los filtros seleccionados
+//*****AQUI  se leen los filtros seleccionados*****//
 
    let checks = [];
-   checks = document.getElementsByTagName("input"); 
+   checks = document.getElementsByTagName("input");//asigna a la variable todos los elementos input en html
 
-   for (let i = 0; i < checks.length; i++) {
-    checks[i].addEventListener("change", checkOptions);     
+   for (let i = 0; i < checks.length; i++) {//Revisa todos los input por algun cambio
+    checks[i].addEventListener("change", checkOptions);// y si lo hay manda a llamar una función  
   }
   
   let filterName = [];
   let filterClassName = []; 
   let filterSelected = [];
   let orderSelected = [];
+
+  //*****Función de filtrado y ordenado*****//
   function checkOptions(event){
-    //console.log(event);
-     filterSelected = (event.target.value);
-     orderSelected = (event.target.value);
-     //console.log(filterSelected);
-     filterClassName = (event.target.className);
-     filterName = (event.target.name);
-    //console.log(filterClassName);
+     filterSelected = (event.target.value);//Guarda el valor del filtro seleccionado
+     orderSelected = (event.target.value);//Guarda el valor del filtro seleccionado
+     filterClassName = (event.target.className);//Guarda el class del elemento seleccionado
+     filterName = (event.target.name);//Guarda el name del elemento seleccionado para filtrar
     if (filterClassName==="order") {
-      selectedData = order(orderSelected, datas);
-      showFIlterOrOrderSearch();
+      selectedData = order(orderSelected, datas);//Asigna el resultado de la función a la variable selectedData
+      showFIlterOrOrderSearch();//Manda a llamar a la función que muestra el contenido ya filtrado/ordenado y search
     }else if (filterName==="filter"){
       selectedData = filter(filterSelected, filterClassName, datas);
       showFIlterOrOrderSearch();  
     } else if (filterName==="stadistic"){ 
       selectedData = stadistics(filterSelected, datas);
-      showStadistic();
+      showStadistic();//Manda a llamar a la función que hace el calculo estadistico y lo muestra en la página
         
      }else if (filterName==="search"){ 
-      const textForSearch = document.getElementById("searchText").value;
+      const textForSearch = document.getElementById("searchText").value;//Guarda el valor obtenido en la variable 
       selectedData = search(textForSearch, filterSelected, datas);
-      
-      //console.log(textForSearch, filterSelected);
       showFIlterOrOrderSearch(); 
      } 
-    
-
   }
-
-
-
-
+//*****Se le da funcionalidad al boton para limpiar campos*****//
 let clear=document.getElementById("unselect");
-clear.addEventListener("click", unselect);
+clear.addEventListener("click", unselect);//Al hacer click sobre el boton se manda allamar la función unselect
 
+//*****Función que deselecciona los radio buttons y muestra la data original*****//
 function unselect(){
   document.querySelectorAll("input").forEach((input) => input.checked=false);
   fatherNode.innerHTML="";
